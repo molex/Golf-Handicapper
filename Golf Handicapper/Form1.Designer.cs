2f5619fe14e58fb62050cebc7a613ff4e0a3d300
Michael Brown <molex333@gmail.com>
2012-01-26 19:57:06 -0500

    Adding all source files

---
+++ Golf Handicapper/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Golf_Handicapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblRating = new System.Windows.Forms.Label();
+            this.lblSlope = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtRating = new System.Windows.Forms.TextBox();
+            this.txtSlope = new System.Windows.Forms.TextBox();
+            this.btnAddScore = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblAdded = new System.Windows.Forms.Label();
+            this.fileGetter = new System.Windows.Forms.OpenFileDialog();
+            this.fileSaver = new System.Windows.Forms.SaveFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 41);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score";
+            // 
+            // lblRating
+            // 
+            this.lblRating.AutoSize = true;
+            this.lblRating.Location = new System.Drawing.Point(12, 67);
+            this.lblRating.Name = "lblRating";
+            this.lblRating.Size = new System.Drawing.Size(74, 13);
+            this.lblRating.TabIndex = 1;
+            this.lblRating.Text = "Course Rating";
+            // 
+            // lblSlope
+            // 
+            this.lblSlope.AutoSize = true;
+            this.lblSlope.Location = new System.Drawing.Point(12, 93);
+            this.lblSlope.Name = "lblSlope";
+            this.lblSlope.Size = new System.Drawing.Size(70, 13);
+            this.lblSlope.TabIndex = 2;
+            this.lblSlope.Text = "Course Slope";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(112, 38);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 3;
+            // 
+            // txtRating
+            // 
+            this.txtRating.Location = new System.Drawing.Point(112, 67);
+            this.txtRating.Name = "txtRating";
+            this.txtRating.Size = new System.Drawing.Size(100, 20);
+            this.txtRating.TabIndex = 4;
+            // 
+            // txtSlope
+            // 
+            this.txtSlope.Location = new System.Drawing.Point(112, 93);
+            this.txtSlope.Name = "txtSlope";
+            this.txtSlope.Size = new System.Drawing.Size(100, 20);
+            this.txtSlope.TabIndex = 5;
+            // 
+            // btnAddScore
+            // 
+            this.btnAddScore.Location = new System.Drawing.Point(13, 131);
+            this.btnAddScore.Name = "btnAddScore";
+            this.btnAddScore.Size = new System.Drawing.Size(132, 23);
+            this.btnAddScore.TabIndex = 6;
+            this.btnAddScore.Text = "Submit Score";
+            this.btnAddScore.UseVisualStyleBackColor = true;
+            this.btnAddScore.Click += new System.EventHandler(this.btnAddScore_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(148, 131);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(132, 23);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate Handicap";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(193, 158);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear ";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem1});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(114, 22);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // lblAdded
+            // 
+            this.lblAdded.AutoSize = true;
+            this.lblAdded.Location = new System.Drawing.Point(71, 185);
+            this.lblAdded.Name = "lblAdded";
+            this.lblAdded.Size = new System.Drawing.Size(19, 13);
+            this.lblAdded.TabIndex = 10;
+            this.lblAdded.Text = "    ";
+            // 
+            // fileGetter
+            // 
+            this.fileGetter.FileName = "openFileDialog1";
+            this.fileGetter.Filter = "Text files (*.txt)|*.txt";
+            this.fileGetter.FileOk += new System.ComponentModel.CancelEventHandler(this.fileGetter_FileOk);
+            // 
+            // fileSaver
+            // 
+            this.fileSaver.Filter = "Text files (*.txt)|*.txt";
+            this.fileSaver.FileOk += new System.ComponentModel.CancelEventHandler(this.fileSaver_FileOk);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(15, 161);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save ";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(104, 158);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 12;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 199);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblAdded);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnAddScore);
+            this.Controls.Add(this.txtSlope);
+            this.Controls.Add(this.txtRating);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblSlope);
+            this.Controls.Add(this.lblRating);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Golf HandiCapper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblRating;
+        private System.Windows.Forms.Label lblSlope;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtRating;
+        private System.Windows.Forms.TextBox txtSlope;
+        private System.Windows.Forms.Button btnAddScore;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.Label lblAdded;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog fileGetter;
+        private System.Windows.Forms.SaveFileDialog fileSaver;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Golf Handicapper/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Golf_Handicapper
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;
+        ArrayList scoreDiff = new ArrayList();
+        double bestScore;
+        double handicap;
+        string strHandicap; 
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private void txtScore_KeyPress(object sender, KeyPressEventArgs e) 
+        {
+            if (!char.IsControl(e.KeyChar) 
+                && !char.IsDigit(e.KeyChar) 
+                && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if (e.KeyChar == '.' 
+                && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+       }
+
+        private void txtRating_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                && !char.IsDigit(e.KeyChar)
+                && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if (e.KeyChar == '.'
+                && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtSlope_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                && !char.IsDigit(e.KeyChar)
+                && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if (e.KeyChar == '.'
+                && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnAddScore_Click(object sender, EventArgs e)
+        {
+            
+            double score = Double.Parse(txtScore.Text);
+            double slope = Double.Parse(txtSlope.Text);
+            double rating = Double.Parse(txtRating.Text);
+
+            double adjustedScore = ((score - rating) * (113 / slope));
+
+            if (count <= 20)
+            {
+                scoreDiff.Add(adjustedScore);
+                count++;
+                txtScore.Text = "";
+                lblAdded.Text = "Score Added " + scoreDiff.Count.ToString() + " Total Score(s)" ;
+            }
+
+            else
+            {
+                count = count - 20;
+                scoreDiff.Add(adjustedScore);
+                count++;
+                txtScore.Text = "";
+                lblAdded.Text = "Score Added " + scoreDiff.Count.ToString() + " Total Score(s)";
+                
+            }
+
+           
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtScore.Text = "";
+            txtSlope.Text = "";
+            txtRating.Text = "";
+            scoreDiff.Clear();
+			lblAdded.Text = "     ";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int i = count - 1;
+            double[] tempSorted = new Double[i];
+            scoreDiff.CopyTo(tempSorted);
+            Array.Sort(tempSorted);
+                         
+            
+            if (scoreDiff.Count <= 5 || scoreDiff.Count == 6)
+            {
+
+                bestScore = tempSorted[0];
+                
+            }
+            else if (scoreDiff.Count == 7 || scoreDiff.Count == 8)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1]) / 2);
+                
+            }
+            else if (scoreDiff.Count == 9 || scoreDiff.Count == 10)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2]) / 3);
+               
+            }
+            else if (scoreDiff.Count == 11 || scoreDiff.Count == 12)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2] + tempSorted[3]) / 4);
+                
+            }
+            else if (scoreDiff.Count == 13 || scoreDiff.Count == 14)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2] + tempSorted[3] + tempSorted[4]) / 5);
+                
+            }
+            else if (scoreDiff.Count == 15 || scoreDiff.Count == 16)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2] + tempSorted[3] + tempSorted[4] + tempSorted[5]) / 6);
+                
+            }
+            else if (scoreDiff.Count == 17)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2] + tempSorted[3] + tempSorted[4] + tempSorted[5] + tempSorted[6]) / 7);
+                
+            }
+            else if (scoreDiff.Count == 18)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2] + tempSorted[3] + tempSorted[4] + tempSorted[5] + tempSorted[6] + tempSorted[7]) / 8);
+                
+            }
+            else if (scoreDiff.Count == 19)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2] + tempSorted[3] + tempSorted[4] + tempSorted[5] + tempSorted[6] + tempSorted[7] + tempSorted[8]) / 9);
+                
+            }
+            else if (scoreDiff.Count >= 20)
+            {
+
+                bestScore = ((tempSorted[0] + tempSorted[1] + tempSorted[2] + tempSorted[3] + tempSorted[4] + tempSorted[5] + tempSorted[6] + tempSorted[7] + tempSorted[8] + tempSorted[9]) / 10);
+                
+            }
+            else
+            {
+                MessageBox.Show("This should not happen!!");
+            }
+            handicap = (bestScore * .96);
+            strHandicap = String.Format("{0:0}", handicap);
+            
+            lblAdded.Text = ("Your  Handicap is: " + strHandicap); 
+			
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fileSaver.ShowDialog();
+            
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fileGetter.ShowDialog();
+        }
+
+        private void fileSaver_FileOk(object sender, CancelEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(fileSaver.FileName);
+            foreach (Double item in scoreDiff)
+            {
+                sw.WriteLine(item);
+            }
+            sw.Close();
+        }
+
+        private void fileGetter_FileOk(object sender, CancelEventArgs e)
+        {
+            StreamReader sr = new StreamReader(fileGetter.FileName);
+            count = 1;
+            string input =  null;
+            double dblInput = 0.00;
+            while ((input = sr.ReadLine()) != null)
+            {
+                dblInput = double.Parse(input);
+                scoreDiff.Add(dblInput);
+                count++;
+            }
+            sr.Close();
+        }
+
+        
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            fileGetter.ShowDialog();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            fileSaver.ShowDialog();
+        }
+    }
+}
